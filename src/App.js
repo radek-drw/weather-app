fac3a555547ebc0fbf23331736c3d65b0da57634
Radek Drweski <rdrweski@gmail.com>
2024-06-22 12:46:18 +0100

    add ThemeProvider and Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
+
 import styled from "styled-components";
-import GlobalStyle from "./GlobalStyles";
+import { theme, GlobalStyle } from "./styles/index";
+import { ThemeProvider } from "styled-components";
+
+import Navbar from "./components/Navbar";
 
 const AppContainer = styled.div`
   width: 70vw;
   height: 80vh;
   border: 1px solid white;
+  padding: 1rem 2rem;
 `;
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <AppContainer></AppContainer>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <AppContainer>
+          <Navbar />
+        </AppContainer>
+      </ThemeProvider>
     </>
   );
 };
