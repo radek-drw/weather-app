4cd471086d04f9561f9e9361e6fdca5d28691cfb
Radek Drweski <rdrweski@gmail.com>
2024-06-27 04:42:03 +0100

    add styles

--- src/components/WeatherData/Subcomponents/WeatherHourly.js
+++ src/components/WeatherData/Subcomponents/WeatherHourly.js
@@ -1,22 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 
+import { CiCloudSun } from "react-icons/ci";
+import { CiLocationArrow1 } from "react-icons/ci";
+
 const HourlyCard = styled.div`
   flex-basis: 65%;
+  display: flex;
+  flex-direction: column;
 `;
 
-const Title = styled.h3``;
+const Title = styled.h3`
+  font-size: 1.6rem;
+  text-align: center;
+  margin-bottom: 15px;
+`;
 
-const HourlyContainer = styled.div``;
+const HourlyContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+`;
 
-const HourlyItem = styled.div``;
+const HourlyItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1e2127;
+  border-radius: 15px;
+  padding: 1rem 0.5rem;
+  flex-basis: 18%;
+`;
 
 const TimeLabel = styled.span``;
 
+const SkyCondIcon = styled(CiCloudSun)`
+  font-size: 28px;
+  flex-basis: 20%;
+`;
+
 const TemperatureValue = styled.span``;
 
-const WindSpeedIndicator = styled.span``;
-
+const WindSpeedIndicator = styled(CiLocationArrow1)`
+  font-size: 22px;
+  flex-basis: 20%;
+`;
 const WindSpeedLabel = styled.span``;
 
 const WeatherHourly = () => {
@@ -26,37 +55,37 @@ const WeatherHourly = () => {
       <HourlyContainer>
         <HourlyItem>
           <TimeLabel>12:00</TimeLabel>
-          {/* icon here */}
+          <SkyCondIcon />
           <TemperatureValue>26&deg;C</TemperatureValue>
-          <WindSpeedIndicator>{/* icon here */}</WindSpeedIndicator>
+          <WindSpeedIndicator />
           <WindSpeedLabel>3km/h</WindSpeedLabel>
         </HourlyItem>
         <HourlyItem>
           <TimeLabel>15:00</TimeLabel>
-          {/* icon here */}
+          <SkyCondIcon />
           <TemperatureValue>27&deg;C</TemperatureValue>
-          <WindSpeedIndicator>{/* icon here */}</WindSpeedIndicator>
+          <WindSpeedIndicator />
           <WindSpeedLabel>2km/h</WindSpeedLabel>
         </HourlyItem>
         <HourlyItem>
           <TimeLabel>18:00</TimeLabel>
-          {/* icon here */}
+          <SkyCondIcon />
           <TemperatureValue>27&deg;C</TemperatureValue>
-          <WindSpeedIndicator>{/* icon here */}</WindSpeedIndicator>
+          <WindSpeedIndicator />
           <WindSpeedLabel>2km/h</WindSpeedLabel>
         </HourlyItem>
         <HourlyItem>
           <TimeLabel>21:00</TimeLabel>
-          {/* icon here */}
+          <SkyCondIcon />
           <TemperatureValue>25&deg;C</TemperatureValue>
-          <WindSpeedIndicator>{/* icon here */}</WindSpeedIndicator>
+          <WindSpeedIndicator />
           <WindSpeedLabel>3km/h</WindSpeedLabel>
         </HourlyItem>
         <HourlyItem>
           <TimeLabel>00:00</TimeLabel>
-          {/* icon here */}
+          <SkyCondIcon />
           <TemperatureValue>22&deg;C</TemperatureValue>
-          <WindSpeedIndicator>{/* icon here */}</WindSpeedIndicator>
+          <WindSpeedIndicator />
           <WindSpeedLabel>3km/h</WindSpeedLabel>
         </HourlyItem>
       </HourlyContainer>
