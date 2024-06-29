121021e36c52002bbc169027417f2642e3fabf09
Radek Drweski <rdrweski@gmail.com>
2024-06-29 23:29:32 +0100

    remove helping comments while creating component

--- src/themeContext.js
+++ src/themeContext.js
@@ -1,17 +1,11 @@
 import React, { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext();
+export const useTheme = () => useContext(ThemeContext);
 
-// FOR DEFAULT LIGHT MODE WHEN APP OPENS UNCOMMENT 'Default light mode', comment 'Default dark mode' and change 'useState' to 'light'
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("dark");
 
-  // -----Default light mode-----
-  // const toggleTheme = () => {
-  //   setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  // };
-
-  // -----Default dark mode-----
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
@@ -21,12 +15,4 @@ export const ThemeProvider = ({ children }) => {
       {children}
     </ThemeContext.Provider>
   );
-};
-
-export const useTheme = () => {
-  const context = useContext(ThemeContext);
-  if (context === undefined) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-  return context;
 };
