ad0267bdecf57524ddae1655ec2494a6f78fdc77
Radek Drweski <rdrweski@gmail.com>
2024-06-29 14:53:50 +0100

    add description how to change default dark/light mode when app starts

--- src/themeContext.js
+++ src/themeContext.js
@@ -2,15 +2,19 @@ import React, { createContext, useContext, useState } from "react";
 
 const ThemeContext = createContext();
 
+// FOR DEFAULT LIGHT MODE WHEN APP OPENS UNCOMMENT 'Default light mode', comment 'Default dark mode' and change 'useState' to 'light'
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
+  // -----Default light mode-----
   // const toggleTheme = () => {
-  //   setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  //   setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   // };
+
+  // -----Default dark mode-----
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
+  };
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
