8a29b6854c43a3c3d364b8886f3f68df250c6d6c
Radek Drweski <rdrweski@gmail.com>
2024-06-29 23:29:56 +0100

    update in import file name

--- src/components/subcomponent/Toggle.js
+++ src/components/subcomponent/Toggle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { useTheme } from "../../themeContext";
+import { useTheme } from "../../ThemeContext";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 const ToggleContainer = styled.button`
