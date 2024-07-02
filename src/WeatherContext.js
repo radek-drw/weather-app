64c103174b7304dadebf01f2b4c0f2558645bf2b
Radek Drweski <rdrweski@gmail.com>
2024-07-02 06:03:04 +0100

    add feature to fetch data by coordinates to the 'Current location' button and when app is first time loaded

--- src/WeatherContext.js
+++ src/WeatherContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 
 const API_KEY = "0268633fae299db526aed6ff3c00d40d";
@@ -7,7 +7,7 @@ const WeatherContext = createContext();
 export const useWeather = () => useContext(WeatherContext);
 
 export const WeatherProvider = ({ children }) => {
-  const [weatherData, setWeatherData] = useState(null);
+  const [weatherData, setWeatherData] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -32,9 +32,46 @@ export const WeatherProvider = ({ children }) => {
     }
   };
 
+  const fetchWeatherByCoordinates = async (latitude, longitude) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      setError("Error fetching weather data by coordinates.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          fetchWeatherByCoordinates(latitude, longitude);
+        },
+        (err) => {
+          setError("Unable to retrieve your location.");
+        }
+      );
+    } else {
+      setError("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <WeatherContext.Provider
-      value={{ weatherData, loading, error, fetchWeatherData }}
+      value={{
+        weatherData,
+        loading,
+        error,
+        fetchWeatherData,
+        fetchWeatherByCoordinates,
+      }}
     >
       {children}
     </WeatherContext.Provider>
