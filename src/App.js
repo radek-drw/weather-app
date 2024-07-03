c87d5c406353c17a84f2b39a178f533c7c1c06e0
Radek Drweski <rdrweski@gmail.com>
2024-07-03 17:54:16 +0100

    seaparate LoadingOverlay to the component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ import {
   useTheme,
 } from "./ThemeContext";
 import { WeatherProvider } from "./WeatherContext";
-import { useWeather } from "./WeatherContext";
 import { lightTheme } from "./styles/themes/lightTheme";
 import { darkTheme } from "./styles/themes/darkTheme";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import { BeatLoader } from "react-spinners";
 import media from "./styles/media";
+import LoadingOverlay from "./components/subcomponent/LoadingOverlay";
 
 import Navbar from "./components/Navbar";
 import WeatherDataContainer from "./components/WeatherDataContainer";
@@ -26,48 +25,27 @@ const AppContainer = styled.div`
   `}
 `;
 
-const LoadingOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 3rem;
-  background: rgba(0, 0, 0, 0.2);
-  z-index: 10;
-  backdrop-filter: blur(3px);
-`;
-
 const ThemedApp = () => {
   const { theme } = useTheme();
   const currentTheme = theme === "dark" ? darkTheme : lightTheme;
 
-  const { loading } = useWeather();
-
   return (
     <ThemeProvider theme={currentTheme}>
+      <WeatherProvider>
       <GlobalStyle />
       <AppContainer>
-        {loading && (
-          <LoadingOverlay>
-            <BeatLoader color="#00aaff" size={20} />
-          </LoadingOverlay>
-        )}
+        <LoadingOverlay/>
         <Navbar />
         <WeatherDataContainer />
       </AppContainer>
+      </WeatherProvider>
     </ThemeProvider>
   );
 };
 
 const App = () => (
   <ThemeContextProvider>
-    <WeatherProvider>
       <ThemedApp />
-    </WeatherProvider>
   </ThemeContextProvider>
 );
 
---
+++ src/components/subcomponent/LoadingOverlay.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import { useWeather } from "../../WeatherContext";
+import { BeatLoader } from "react-spinners";
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  background: rgba(0, 0, 0, 0.2);
+  z-index: 10;
+  backdrop-filter: blur(3px);
+`;
+
+const LoadingOverlay = () => {
+    const {loading} = useWeather();
+    
+    return (
+        loading && (
+        <Overlay>
+            <BeatLoader color="#00aaff" size={20} />
+        </Overlay>
+        )
+    );
+};
+
+export default LoadingOverlay;
