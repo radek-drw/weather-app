fb178eb8722c5e8d089856af63646c62a47eeca1
Radek Drweski <rdrweski@gmail.com>
2024-07-03 16:38:54 +0100

    add 'LoadingOverlay' to cover whole app while fetching data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styled from "styled-components";
 import { ThemeProvider } from "styled-components";
 import {
@@ -7,9 +6,11 @@ import {
   useTheme,
 } from "./ThemeContext";
 import { WeatherProvider } from "./WeatherContext";
+import { useWeather } from "./WeatherContext";
 import { lightTheme } from "./styles/themes/lightTheme";
 import { darkTheme } from "./styles/themes/darkTheme";
 import { GlobalStyle } from "./styles/GlobalStyle";
+import { BeatLoader } from "react-spinners";
 import media from "./styles/media";
 
 import Navbar from "./components/Navbar";
@@ -25,26 +26,48 @@ const AppContainer = styled.div`
   `}
 `;
 
+const LoadingOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
+  background: rgba(0, 0, 0, 0.2);
+  z-index: 10;
+  backdrop-filter: blur(3px);
+`;
+
 const ThemedApp = () => {
   const { theme } = useTheme();
   const currentTheme = theme === "dark" ? darkTheme : lightTheme;
 
+  const { loading } = useWeather();
+
   return (
     <ThemeProvider theme={currentTheme}>
-      <WeatherProvider>
-        <GlobalStyle />
-        <AppContainer>
-          <Navbar />
-          <WeatherDataContainer />
-        </AppContainer>
-      </WeatherProvider>
+      <GlobalStyle />
+      <AppContainer>
+        {loading && (
+          <LoadingOverlay>
+            <BeatLoader color="#00aaff" size={20} />
+          </LoadingOverlay>
+        )}
+        <Navbar />
+        <WeatherDataContainer />
+      </AppContainer>
     </ThemeProvider>
   );
 };
 
 const App = () => (
   <ThemeContextProvider>
-    <ThemedApp />
+    <WeatherProvider>
+      <ThemedApp />
+    </WeatherProvider>
   </ThemeContextProvider>
 );
 
