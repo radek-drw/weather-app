f8da812bf30d49bd3ace518ab1e1ad00c72b81b0
Radek Drweski <rdrweski@gmail.com>
2024-07-05 15:05:13 +0100

    remove helping comments

--- src/utils/WeatherIcons.js
+++ src/utils/WeatherIcons.js
@@ -3,7 +3,7 @@ import clearNightIcon from './weather_icons/clear_night.svg';
 import fewCloudsDayIcon from './weather_icons/few_clouds_day.svg';
 import fewCloudsNightIcon from './weather_icons/few_clouds_night.svg';
 import scatteredCloudsIcon from './weather_icons/scattered_clouds.svg';
-import brokenCloudsIcon from './weather_icons/broken_clouds.svg';
+// import brokenCloudsIcon from './weather_icons/broken_clouds.svg';
 import showerRainDayIcon from './weather_icons/shower_rain_day.svg';
 import showerRainNightIcon from './weather_icons/shower_rain_night.svg';
 import rainIcon from './weather_icons/rain.svg';
@@ -14,22 +14,22 @@ import mistIcon from './weather_icons/mist.svg';
 
   
 export const weatherIcons = {
-  "01d": sunnyDayIcon, // clear sky day
-  "01n": clearNightIcon, // clear sky night
-  "02d": fewCloudsDayIcon, // few clouds day
-  "02n": fewCloudsNightIcon, // few clouds night
-  "03d": scatteredCloudsIcon, // scattered clouds day
-  "03n": scatteredCloudsIcon, // scattered clouds night
-  "04d": brokenCloudsIcon, // broken clouds day
-  "04n": brokenCloudsIcon, // broken clouds night
-  "09d": showerRainDayIcon, // shower rain day
-  "09n": showerRainNightIcon, // shower rain night
-  "10d": rainIcon, // rain day
-  "10n": rainIcon, // rain night
-  "11d": thunderstormDayIcon, // thunderstorm day
-  "11n": thunderstormNightIcon, // thunderstorm night
-  "13d": snowIcon, // snow day
-  "13n": snowIcon, // snow night
-  "50d": mistIcon, // mist day
-  "50n": mistIcon, // mist night
+  "01d": sunnyDayIcon, 
+  "01n": clearNightIcon, 
+  "02d": fewCloudsDayIcon, 
+  "02n": fewCloudsNightIcon,
+  "03d": scatteredCloudsIcon, 
+  "03n": scatteredCloudsIcon, 
+//   "04d": brokenCloudsIcon, // broken clouds day
+//   "04n": brokenCloudsIcon, // broken clouds night
+  "09d": showerRainDayIcon,
+  "09n": showerRainNightIcon, 
+  "10d": rainIcon,
+  "10n": rainIcon,
+  "11d": thunderstormDayIcon, 
+  "11n": thunderstormNightIcon, 
+  "13d": snowIcon, 
+  "13n": snowIcon, 
+  "50d": mistIcon, 
+  "50n": mistIcon, 
 };
