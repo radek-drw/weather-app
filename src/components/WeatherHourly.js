66470bca604ff41302f1130d1f50d00196502a1f
Radek Drweski <rdrweski@gmail.com>
2024-07-06 02:10:34 +0100

    add hourly forecast

--- src/components/WeatherHourly.js
+++ src/components/WeatherHourly.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { CiCloudSun } from "react-icons/ci";
-import { CiLocationArrow1 } from "react-icons/ci";
+import { FaLocationArrow } from "react-icons/fa";
+import { useWeather } from "../WeatherContext";
+import { weatherIcons } from "../utils/weatherIcons";
 
 const HourlyCard = styled.div`
   flex-basis: 65%;
@@ -32,56 +33,39 @@ const HourlyItem = styled.div`
   border-radius: 15px;
 `;
 
-const SkyCondIcon = styled(CiCloudSun)`
-  flex-basis: 20%;
-  font-size: 28px;
+const WeatherIcon = styled.img`
+  width: 34px;
+  height: 34px;
 `;
 
-const WindSpeedIndicator = styled(CiLocationArrow1)`
-  flex-basis: 20%;
-  font-size: 22px;
+const WindSpeedIndicator = styled(FaLocationArrow)`
+  font-size: 15px;
+  color: #00aaff;
 `;
 
 const WeatherHourly = () => {
+  const { weatherData, forecastData } = useWeather();
+
+  const cityTimezoneOffset = weatherData.timezone;
+  const localTime = (unixUtcTimestamp) => {
+    const dt = new Date(unixUtcTimestamp * 1000);
+    dt.setUTCSeconds(dt.getUTCSeconds() + cityTimezoneOffset);
+    return dt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  };
+
   return (
     <HourlyCard>
       <Title>Hourly Forecast</Title>
       <HourlyContainer>
-        <HourlyItem>
-          <span>12:00</span>
-          <SkyCondIcon />
-          <span>26&deg;C</span>
-          <WindSpeedIndicator />
-          <span>3km/h</span>
-        </HourlyItem>
-        <HourlyItem>
-          <span>15:00</span>
-          <SkyCondIcon />
-          <span>27&deg;C</span>
-          <WindSpeedIndicator />
-          <span>2km/h</span>
-        </HourlyItem>
-        <HourlyItem>
-          <span>18:00</span>
-          <SkyCondIcon />
-          <span>27&deg;C</span>
-          <WindSpeedIndicator />
-          <span>2km/h</span>
-        </HourlyItem>
-        <HourlyItem>
-          <span>21:00</span>
-          <SkyCondIcon />
-          <span>25&deg;C</span>
-          <WindSpeedIndicator />
-          <span>3km/h</span>
-        </HourlyItem>
-        <HourlyItem>
-          <span>00:00</span>
-          <SkyCondIcon />
-          <span>22&deg;C</span>
-          <WindSpeedIndicator />
-          <span>3km/h</span>
-        </HourlyItem>
+        {forecastData.slice(0, 5).map((forecast, index) => (
+          <HourlyItem key={index}>
+            <span>{localTime(forecast.dt)}</span>
+            <WeatherIcon src={weatherIcons[forecast.weather[0].icon]} alt={forecast.weather[0].description} />
+            <span>{Math.round(forecast.main.temp)}&deg;C</span>
+            <WindSpeedIndicator style={{ transform: `rotate(${forecast.wind.deg}deg)` }} />
+            <span>{Math.round(forecast.wind.speed)}km/h</span>
+          </HourlyItem>
+        ))}
       </HourlyContainer>
     </HourlyCard>
   );
