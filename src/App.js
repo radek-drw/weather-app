d0719bd7f1599bcb409d962019c376120e827f18
Radek Drweski <rdrweski@gmail.com>
2024-07-17 18:21:04 +0100

    move 'Loading overlay' and 'Errors fetching data window' to DataContainer for better maintanance and clean up App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import {
   ThemeProvider as ThemeContextProvider,
   useTheme,
 } from "./ThemeContext";
-import { WeatherProvider, useWeather } from "./WeatherContext";
+import { WeatherProvider } from "./WeatherContext";
 import { lightTheme } from "./styles/themes/lightTheme";
 import { darkTheme } from "./styles/themes/darkTheme";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import media from "./styles/media";
-import LoadingOverlay from "./components/subcomponent/LoadingOverlay";
 import Navbar from "./components/Navbar";
 import WeatherDataContainer from "./components/WeatherDataContainer";
-import { FaExclamationTriangle } from "react-icons/fa";
+
 
 const AppContainer = styled.div`
   width: 70vw;
@@ -22,28 +21,6 @@ const AppContainer = styled.div`
   ${media.tablet`
     width: 100vw;
   `}
-`;
-
-const ErrorContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 1.6rem;
-  text-align: center;
-  color: orange;
-  height: 65vh;
-  padding: 3rem;
-  margin-top: 20px;
-  border: 1px solid orange;
-  border-radius: 8px;
-  background-color: rgba(255, 165, 0, 0.1);
-`;
-
-const ErrorIcon = styled(FaExclamationTriangle)`
-  font-size: 2.6rem;
-  color: orange;
-  margin-top: 10px;
 `;
 
 const ThemedApp = () => {
@@ -61,23 +38,11 @@ const ThemedApp = () => {
 };
 
 const AppContent = () => {
-  const { error } = useWeather();
 
   return (
     <AppContainer>
       <Navbar />
-      {error ? (
-        <>
-          <ErrorContainer>{error}
-            <ErrorIcon />
-          </ErrorContainer> 
-        </>
-      ) : (
-        <>
-          <LoadingOverlay />
-          <WeatherDataContainer />
-        </>
-      )}
+      <WeatherDataContainer />
     </AppContainer>
   );
 };
--- src/components/WeatherDataContainer.js
+++ src/components/WeatherDataContainer.js
@@ -1,12 +1,13 @@
 import React from "react";
-
 import styled from "styled-components";
 import media from "../styles/media";
-
+import { useWeather } from "../WeatherContext";
 import WeatherCurrentCity from "./WeatherCurrentCity";
 import WeatherDetails from "./WeatherDetails";
 import Weather5day from "./Weather5day";
 import WeatherHourly from "./WeatherHourly";
+import LoadingOverlay from "./subcomponent/LoadingOverlay";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const Main = styled.main`
   display: flex;
@@ -27,15 +28,52 @@ const Main = styled.main`
     box-shadow: 0 4px 8px ${({ theme }) => theme.colors.cardShadow};
 
     ${media.mobile`
-    flex-basis: 100%;
-    padding: 15px 0;
-    box-shadow: 0 4px 4px -4px ${({ theme }) => theme.colors.cardShadow};
-    border-radius: 0;
-  `}
+      flex-basis: 100%;
+      padding: 15px 0;
+      box-shadow: 0 4px 4px -4px ${({ theme }) => theme.colors.cardShadow};
+      border-radius: 0;
+    `}
   }
 `;
 
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.6rem;
+  text-align: center;
+  color: orange;
+  height: 60vh;
+  padding: 3rem;
+  margin-top: 20px;
+  border: 1px solid orange;
+  border-radius: 8px;
+  background-color: rgba(255, 165, 0, 0.1);
+`;
+
+const ErrorIcon = styled(FaExclamationTriangle)`
+  font-size: 2.6rem;
+  color: orange;
+  margin-top: 10px;
+`;
+
 const WeatherDataContainer = () => {
+  const { error, loading } = useWeather();
+
+  if (loading) {
+    return <LoadingOverlay />;
+  }
+
+  if (error) {
+    return (
+      <ErrorContainer>
+        {error}
+        <ErrorIcon/>
+      </ErrorContainer>
+    );
+  }
+
   return (
     <Main>
       <WeatherCurrentCity />
