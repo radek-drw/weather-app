2b10b01d512376d7ed6c2690371ecb73cb20b9e2
Radek Drweski <rdrweski@gmail.com>
2024-07-19 16:42:24 +0100

    Move the mapping of forecastData to a variable hourlyForecasts to simplify the return statement

--- src/components/WeatherHourly/WeatherHourly.js
+++ src/components/WeatherHourly/WeatherHourly.js
@@ -12,34 +12,51 @@ import {
   WindSpeedIndicator
 } from './WeatherHourly.styles';
 
+// Utility function to convert Unix UTC timestamp to local time
+const toLocalTime = (unixUtcTimestamp, timezoneOffset) => {
+  const date = new Date(unixUtcTimestamp * 1000);
+  date.setUTCSeconds(date.getUTCSeconds() + timezoneOffset);
+  return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+};
+
 const WeatherHourly = () => {
   const { weatherData, forecastData, tempUnit } = useWeather();
   const { t } = useTranslation();
 
   if (!weatherData || !forecastData) {
-    return null
+    return null;
   }
 
-  const cityTimezoneOffset = weatherData.timezone;
-  const localTime = (unixUtcTimestamp) => {
-    const dt = new Date(unixUtcTimestamp * 1000);
-    dt.setUTCSeconds(dt.getUTCSeconds() + cityTimezoneOffset);
-    return dt.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  };
+  const { timezone: cityTimezoneOffset } = weatherData;
+
+  // Slice the forecast data and map it to hourly items
+  const hourlyForecasts = forecastData.slice(0, 5).map((forecast, index) => {
+    const { dt, weather, main, wind } = forecast;
+    const time = toLocalTime(dt, cityTimezoneOffset);
+    const temperature = Math.round(main.temp);
+    const windSpeed = Math.round(wind.speed);
+    const windDirection = wind.deg;
+    const iconSrc = weatherIcons[weather[0].icon];
+    const iconAlt = weather[0].description;
+    const temperatureUnit = tempUnit === 'metric' ? 'C' : 'F';
+    const windStyle = { transform: `rotate(${windDirection}deg)` };
+
+    return (
+      <HourlyItem key={index}>
+        <span>{time}</span>
+        <WeatherIcon src={iconSrc} alt={iconAlt} />
+        <span>{temperature}&deg;{temperatureUnit}</span>
+        <WindSpeedIndicator style={windStyle} />
+        <span>{windSpeed}km/h</span>
+      </HourlyItem>
+    );
+  });
 
   return (
     <HourlyCard>
       <Title>{t('labels.hourlyForecastTitle')}</Title>
       <HourlyContainer>
-        {forecastData.slice(0, 5).map((forecast, index) => (
-          <HourlyItem key={index}>
-            <span>{localTime(forecast.dt)}</span>
-            <WeatherIcon src={weatherIcons[forecast.weather[0].icon]} alt={forecast.weather[0].description} />
-            <span>{Math.round(forecast.main.temp)}&deg;{tempUnit === 'metric' ? 'C' : 'F'}</span>
-            <WindSpeedIndicator style={{ transform: `rotate(${forecast.wind.deg}deg)` }} />
-            <span>{Math.round(forecast.wind.speed)}km/h</span>
-          </HourlyItem>
-        ))}
+        {hourlyForecasts}
       </HourlyContainer>
     </HourlyCard>
   );
