8b4afcebf795c45b3ad6fb14a5bc1e4d562121b2
Radek Drweski <rdrweski@gmail.com>
2024-07-19 16:50:30 +0100

    Simplify component to focus on rendering logic and avoid inline function definitions

--- src/components/Weather5day/Weather5day.js
+++ src/components/Weather5day/Weather5day.js
@@ -11,42 +11,47 @@ import {
   DateValue
 } from './Weather5day.styles';
 
+const formatDate = (dateString, locale) => {
+  const dateTime = new Date(dateString);
+  return dateTime.toLocaleDateString(locale, { 
+    weekday: 'short', 
+    day: 'numeric', 
+    month: 'short' 
+  });
+};
+
+const processForecastData = (data, locale) => {
+  const dailyData = {};
+
+  data.forEach((item) => {
+    const date = formatDate(item.dt_txt, locale);
+    const weatherCode = item.weather[0].icon.replace('n', 'd');
+
+    if (!dailyData[date]) {
+      dailyData[date] = {
+        minTemp: item.main.temp_min,
+        maxTemp: item.main.temp_max,
+        weatherCode: weatherCode,
+      };
+    } else {
+      dailyData[date].minTemp = Math.min(dailyData[date].minTemp, item.main.temp_min);
+      dailyData[date].maxTemp = Math.max(dailyData[date].maxTemp, item.main.temp_max);
+    }
+  });
+
+  return Object.keys(dailyData).map((date) => ({
+    date,
+    ...dailyData[date],
+  }));
+};
+
 const Weather5day = () => {
   const { forecastData, tempUnit } = useWeather();
   const { t, i18n } = useTranslation();
 
-  const processForecastData = (data) => {
-    const dailyData = {};
+  const daysForecast = processForecastData(forecastData, i18n.language).slice(0, 5);
 
-    data.forEach((item) => {
-      const dateTime = new Date(item.dt_txt);
-      const date = dateTime.toLocaleDateString(i18n.language, { 
-        weekday: 'short', 
-        day: 'numeric', 
-        month: 'short' 
-      });
-
-      const weatherCode = item.weather[0].icon.replace('n', 'd');
-
-      if (!dailyData[date]) {
-        dailyData[date] = {
-          minTemp: item.main.temp_min,
-          maxTemp: item.main.temp_max,
-          weatherCode: weatherCode,
-        };
-      } else {
-        dailyData[date].minTemp = Math.min(dailyData[date].minTemp, item.main.temp_min);
-        dailyData[date].maxTemp = Math.max(dailyData[date].maxTemp, item.main.temp_max);
-      }
-    });
-
-    return Object.keys(dailyData).map((date) => ({
-      date,
-      ...dailyData[date],
-    }));
-  };
-
-  const daysForecast = processForecastData(forecastData).slice(0, 5);
+  const tempSuffix = tempUnit === 'metric' ? 'C' : 'F';
 
   return (
     <FiveDaysCard>
@@ -55,8 +60,7 @@ const Weather5day = () => {
         <DayContainer key={index}>
           <WeatherIcon src={weatherIcons[forecast.weatherCode]} alt="Weather Icon" />
           <TemperatureValue>
-            {Math.round(forecast.maxTemp)}&deg;{tempUnit === 'metric' ? 'C' : 'F'} /{' '}  
-            {Math.round(forecast.minTemp)}&deg;{tempUnit === 'metric' ? 'C' : 'F'}
+            {Math.round(forecast.maxTemp)}&deg;{tempSuffix} / {Math.round(forecast.minTemp)}&deg;{tempSuffix}
           </TemperatureValue>
           <DateValue>{forecast.date}</DateValue>
         </DayContainer>
