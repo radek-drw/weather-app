e9121011970013ceb881c2515347a18849f1e96f
Radek Drweski <rdrweski@gmail.com>
2024-07-23 05:14:32 +0100

    convert i18n configuration to TypeScript

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,19 +10,23 @@ import itTranslations from './translations/it.json';
 import plTranslations from './translations/pl.json';
 import ptTranslations from './translations/pt.json';
 
+type TranslationResource = typeof enTranslations;
+
+const resources: { [key: string]: { translation: TranslationResource } } = {
+  de: { translation: deTranslations },
+  en: { translation: enTranslations },
+  es: { translation: esTranslations },
+  fr: { translation: frTranslations },
+  it: { translation: itTranslations },
+  pl: { translation: plTranslations },
+  pt: { translation: ptTranslations },
+};
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      de: { translation: deTranslations },
-      en: { translation: enTranslations },
-      es: { translation: esTranslations },
-      fr: { translation: frTranslations },
-      it: { translation: itTranslations },
-      pl: { translation: plTranslations },
-      pt: { translation: ptTranslations },
-    },
+    resources,
     fallbackLng: 'en',
     lng: 'en',
     debug: false,
