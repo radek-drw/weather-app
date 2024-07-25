3e30c921ab90f6d5977533d93520bba59ad31cf6
Radek Drweski <rdrweski@gmail.com>
2024-07-25 17:05:28 +0100

    create Jest configuration for TypeScript support and test environment setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  };
