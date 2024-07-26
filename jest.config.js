8f21285c2d3f3d270d90dd200ddc102a705e9c9f
Radek Drweski <rdrweski@gmail.com>
2024-07-26 17:58:03 +0100

    update configuration for TypeScript support and test environment setup

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,24 @@
+// jest.config.js
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/__mocks__/fileMock.js',
+  },
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    '/node_modules/',
-  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: [
+    '<rootDir>/src/**/*.test.(ts|tsx|js|jsx)',
+    '<rootDir>/tests/**/*.test.(ts|tsx|js|jsx)',
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
 };
