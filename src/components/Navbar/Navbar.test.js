43246b5feb8529d1abffaffd1d1e1c7bbcd7005b
Radek Drweski <rdrweski@gmail.com>
2024-08-02 13:20:19 +0100

    refactor Navbar.test.js and remove unnecessary lines

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { I18nextProvider } from 'react-i18next';
@@ -8,7 +9,6 @@ import i18n from '../../i18n';
 import translations from '../../translations/en.json';
 import { lightTheme, darkTheme } from '../../styles/themes/index';
 
-// Mock the WeatherContext
 jest.mock('../../WeatherContext', () => {
   const fetchWeatherByCoordinates = jest.fn();
   return {
@@ -22,7 +22,6 @@ jest.mock('../../WeatherContext', () => {
   };
 });
 
-// Mock the ThemeContext module
 jest.mock('../../ThemeContext', () => {
   const actualThemeContext = jest.requireActual('../../ThemeContext');
   return {
@@ -31,7 +30,6 @@ jest.mock('../../ThemeContext', () => {
   };
 });
 
-// Function to render Navbar with the required providers
 const renderNavbar = (theme = 'light') => {
   const toggleTheme = jest.fn();
   const { useTheme } = require('../../ThemeContext');
@@ -114,51 +112,6 @@ describe('Navbar Component', () => {
     await waitFor(() => {
       expect(screen.getByText(getText('errors.invalidCityName'))).toBeInTheDocument();
     });
-  });
-
-  // Test for fetching weather by coordinates
-  test('calls fetchWeatherByCoordinates when current location button is clicked', () => {
-    const { fetchWeatherByCoordinates } = require('../../WeatherContext').useWeather();
-
-    renderNavbar();
-    const currentLocationButton = screen.getByTestId('current-location-button');
-
-    global.navigator.geolocation = {
-      getCurrentPosition: jest.fn().mockImplementationOnce((success) => {
-        // Generate random coordinates for testing
-        const latitude = 51.5 + Math.random() * 0.1;
-        const longitude = -0.13 + Math.random() * 0.1;
-        success({ coords: { latitude, longitude } });
-      }),
-    };
-
-    fireEvent.click(currentLocationButton);
-
-    expect(fetchWeatherByCoordinates).toHaveBeenCalledWith(expect.any(Number), expect.any(Number));
-  });
-
-  // Test for checking coordinates range
-  test('calls fetchWeatherByCoordinates with coordinates within expected range', () => {
-    const { fetchWeatherByCoordinates } = require('../../WeatherContext').useWeather();
-
-    renderNavbar();
-    const currentLocationButton = screen.getByTestId('current-location-button');
-
-    global.navigator.geolocation = {
-      getCurrentPosition: jest.fn().mockImplementationOnce((success) => {
-        success({ coords: { latitude: 51.5074, longitude: -0.1278 } });
-      }),
-    };
-
-    fireEvent.click(currentLocationButton);
-
-    const [latitude, longitude] = fetchWeatherByCoordinates.mock.calls[0];
-
-    // Check if latitude and longitude are within a realistic range
-    expect(latitude).toBeGreaterThanOrEqual(51.5);
-    expect(latitude).toBeLessThanOrEqual(51.6);
-    expect(longitude).toBeGreaterThanOrEqual(-0.14);
-    expect(longitude).toBeLessThanOrEqual(-0.12);
   });
 
   // Test for theme toggling
