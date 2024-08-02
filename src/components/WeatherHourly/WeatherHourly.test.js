ef3bff6fee8e15f67d26d41dec03f0aef5e1be64
Radek Drweski <rdrweski@gmail.com>
2024-08-02 13:21:28 +0100

    refactor to import React for unit testing

--- src/components/WeatherHourly/WeatherHourly.test.js
+++ src/components/WeatherHourly/WeatherHourly.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
 import WeatherHourly from './WeatherHourly';
